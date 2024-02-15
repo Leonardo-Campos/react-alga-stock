f5e88f6b0902fdf29985e2ce9bd53c8e3003b1ab
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2024-02-15 16:50:04 -0300

    Ciclos de vida dos componentes

--- alga-stock/src/components/ClassComponents/ClassComponents.tsx
+++ alga-stock/src/components/ClassComponents/ClassComponents.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 
 class ClassComponent extends React.Component<{name: string}> {
+
+    constructor(props: any) {
+        super(props)
+        console.log('constructor reached')
+    }
+
     state = {
         name: 'Mundo'
     }
+
+    componentDidMount(): void {
+        console.log('componentDidMount reached')
+    }
+
+    componentDidUpdate(): void {
+        console.log('componentDidUpdate reached')
+    }
+    
+
     render() {
+        console.log('render reached')
         return <div>
             <p>{this.state.name}</p>
             <button onClick={() => {
