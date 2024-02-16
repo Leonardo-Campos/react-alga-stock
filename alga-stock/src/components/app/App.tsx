80805a22f64dd5c9e7427d7c97848d83d9addb54
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2024-02-16 20:10:12 -0300

    list render

--- alga-stock/src/components/app/App.tsx
+++ alga-stock/src/components/app/App.tsx
@@ -18,18 +18,15 @@ function App() {
       <Header title="AlgaStock" />
 
       <Container>
-        <Button
-          onClick={() => window.alert('UIIIU')}
-          appendIcon={<TestComponent />}
-        >
-          Alert
-        </Button>
-        <Input
-          label="Street"
-          placeholder="E.g.: 15h Avenue"
-          value={street}
-          onChange={e => setStreet(e.target.value)}
-        />
+        <ul>
+        {
+         ['Daniel', 'Willian', 'Thiago'].map((name, index) => {
+          return <li key={index}>
+            { name }
+          </li>
+         })
+        }
+        </ul>
       </Container>
     </div>
   );
