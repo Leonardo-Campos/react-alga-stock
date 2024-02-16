f3180d19c7a37b6001c6303f050cabf4ba6d4149
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2024-02-16 20:00:19 -0300

    add input component

--- alga-stock/src/components/app/App.tsx
+++ alga-stock/src/components/app/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from '../Header';
 import Button from '../../shared/Button';
 import Container from '../../shared/Container';
+import Input from '../../shared/Input';
 
 
 function TestComponent () {
@@ -10,19 +11,27 @@ function TestComponent () {
 }
 
 function App() {
+  const [street, setStreet] = useState('')
+
   return (
     <div className="App">
       <Header title="AlgaStock" />
 
       <Container>
-      <Button
+        <Button
           onClick={() => window.alert('UIIIU')}
           appendIcon={<TestComponent />}
         >
           Alert
         </Button>
+        <Input
+          label="Street"
+          placeholder="E.g.: 15h Avenue"
+          value={street}
+          onChange={e => setStreet(e.target.value)}
+        />
       </Container>
-      </div>
+    </div>
   );
 }
 
