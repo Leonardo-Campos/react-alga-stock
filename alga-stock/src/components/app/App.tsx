e4b9f51ebb39f2c3e8be743b72a4072a926f8e9b
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2024-02-19 14:06:43 -0300

    clear app + add table component's base files

--- alga-stock/src/components/app/App.tsx
+++ alga-stock/src/components/app/App.tsx
@@ -1,14 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import Header from '../Header';
-import Button from '../../shared/Button';
 import Container from '../../shared/Container';
-import Input from '../../shared/Input';
-
-
-function TestComponent () {
-  return <img width="16" src="https://img.icons8.com/pastel-glyph/2x/search--v2.png" alt="search icon" />
-}
 
 function App() {
   const [street, setStreet] = useState('')
@@ -18,15 +11,7 @@ function App() {
       <Header title="AlgaStock" />
 
       <Container>
-        <ul>
-        {
-         ['Daniel', 'Willian', 'Thiago'].map((name, index) => {
-          return <li key={index}>
-            { name }
-          </li>
-         })
-        }
-        </ul>
+        
       </Container>
     </div>
   );
