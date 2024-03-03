9ee23a2fe0bd3002f5134dca5eaee8f575e3105a
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2024-03-03 20:13:21 -0300

    propify table

--- alga-stock/src/components/app/App.tsx
+++ alga-stock/src/components/app/App.tsx
@@ -1,20 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Header from '../Header';
 import Container from '../../shared/Container';
-import Table from '../../shared/Table';
+import Table, { TableHeader } from '../../shared/Table';
+import Products from '../../shared/Table/Table.mockdata';
+
+const headers: TableHeader[] = [
+  { key: 'id', value: '#' },
+  { key: 'name', value: 'Product' },
+  { key: 'price', value: 'Price', right: true },
+  { key: 'stock', value: 'Available Stock', right: true }
+]
 
 function App() {
-    return (
+  return (
     <div className="App">
       <Header title="AlgaStock" />
-
       <Container>
-        <Table />
+        <Table
+          headers={headers}
+          data={Products}
+        />
       </Container>
     </div>
   );
 }
-
 
 export default App;
