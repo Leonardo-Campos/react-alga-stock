f4aefab79fe15ea8dc77fdd712d03f330d9cac82
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2024-03-03 21:01:27 -0300

    form component

--- alga-stock/src/shared/Button/Button.tsx
+++ alga-stock/src/shared/Button/Button.tsx
@@ -1,21 +1,21 @@
-import React from "react";
+import React from 'react'
 import './Button.css'
 
-declare interface BottonProps {
-    content?: string;
-    onClick?: () => void;
-    appendIcon: JSX.Element
-    children: string
+declare interface ButtonProps {
+  content?: string
+  onClick?: () => void
+  appendIcon?: JSX.Element
+  children: string
 }
 
-
-const Button: React.FC<BottonProps> = (props) => {
-    return <button className="AppButton"
+const Button: React.FC<ButtonProps> = (props) => {
+  return <button
+    className="AppButton"
     onClick={props.onClick}
-    >
-        { props.children || 'Nameless Button' }
-        { props.appendIcon }
-    </button>
+  >
+    { props.children || 'Nameless button' }
+    { props.appendIcon }
+  </button>
 }
 
 export default Button
