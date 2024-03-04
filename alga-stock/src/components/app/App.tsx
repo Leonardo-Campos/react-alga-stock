1855c9204cf07fb81f2b9877a41f072f3fe19336
Leonardo-Campos <leonardo.campos.p.lopes@gmail.com>
2024-03-04 19:19:11 -0300

    add product-form component

--- alga-stock/src/components/app/App.tsx
+++ alga-stock/src/components/app/App.tsx
@@ -4,9 +4,7 @@ import Header from '../Header';
 import Container from '../../shared/Container';
 import Table, { TableHeader } from '../../shared/Table';
 import Products from '../../shared/Table/Table.mockdata';
-import Input from '../../shared/Input';
-import Button from '../../shared/Button';
-import Form from '../../shared/Form';
+import ProductForm from '../Product/ProductForm';
 
 const headers: TableHeader[] = [
   { key: 'id', value: '#' },
@@ -16,23 +14,16 @@ const headers: TableHeader[] = [
 ]
 
 function App() {
-  function handleSubmitForm (data: any) {
-    console.log(data)
-  }
   return (
     <div className="App">
-      <Header title="Stock" />
-
+      <Header title="AlgaStock" />
       <Container>
-        <Table headers={headers} data={Products} />
+        <Table
+          headers={headers}
+          data={Products}
+        />
 
-        <Form title="Product form" onSubmit={handleSubmitForm}>
-          <Input label="Name" placeholder="E.g.: Cookie" />
-          <Input label="Price" type="number" step="0.01" min="0" placeholder="E.g.: 1.25" />
-          <Input label="Stock" type="number" min="0" placeholder="E.g.: 15" />
-
-          <Button>Submit</Button>
-        </Form>
+        <ProductForm />
       </Container>
     </div>
   );
